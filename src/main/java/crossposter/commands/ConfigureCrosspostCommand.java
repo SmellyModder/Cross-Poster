2924daf0a3fb1bfd1fda54b58dd8ae77e5391316
SmellyModder <smellysocks543@gmail.com>
2020-12-10 21:31:12 -0500

    Revert "Revert "Implement Disparser from Gradle""
    This reverts commit 9482373c0a075a68c5481a919eb21c2d7d32325d.

--- src/main/java/crossposter/commands/ConfigureCrosspostCommand.java
+++ src/main/java/crossposter/commands/ConfigureCrosspostCommand.java
@@ -3,16 +3,27 @@ package crossposter.commands;
 import crossposter.ServerDataHandler;
 import crossposter.ServerDataHandler.ChannelData;
 import crossposter.ServerDataHandler.ServerData;
-import disparser.Command;
-import disparser.CommandContext;
-import disparser.MessageUtil;
-import disparser.arguments.jda.TextChannelArgument;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.smelly.disparser.Command;
+import net.smelly.disparser.CommandContext;
+import net.smelly.disparser.arguments.jda.TextChannelArgument;
+import net.smelly.disparser.feedback.exceptions.DynamicCommandExceptionCreator;
+import net.smelly.disparser.feedback.exceptions.SimpleCommandExceptionCreator;
 
 public class ConfigureCrosspostCommand extends Command {
+	private static final DynamicCommandExceptionCreator<TextChannel> DOES_NOT_CROSSPOST_TO_EXCEPTION = DynamicCommandExceptionCreator.createInstance(channel -> {
+		return "This channel doesn't crosspost to " + channel.getAsMention();
+	});
+	private static final DynamicCommandExceptionCreator<TextChannel> ALREADY_CROSSPOSTS_TO_EXCEPTION = DynamicCommandExceptionCreator.createInstance(channel -> {
+		if (channel != null) {
+			return channel.getAsMention() + " crossposts to another channel";
+		}
+		return "Channel you want to crosspost to crossposts to another channel";
+	});
+	private static final SimpleCommandExceptionCreator CANNOT_CROSSPOST_TO_SELF_EXCEPTION = new SimpleCommandExceptionCreator("Cannot make this channel crosspost to itself!");
 	private final boolean disable;
 	
 	public ConfigureCrosspostCommand(boolean disable) {
@@ -21,7 +32,7 @@ public class ConfigureCrosspostCommand extends Command {
 	}
 
 	@Override
-	public void processCommand(CommandContext context) {
+	public void processCommand(CommandContext context) throws Exception {
 		GuildMessageReceivedEvent event = context.getEvent();
 		TextChannel channel = event.getChannel();
 		Message message = event.getMessage();
@@ -43,28 +54,26 @@ public class ConfigureCrosspostCommand extends Command {
 				}
 				
 				if (noData) {
-					this.sendMessage(channel, MessageUtil.createErrorMessage("This channel doesn't crosspost to that channel"));
+					throw DOES_NOT_CROSSPOST_TO_EXCEPTION.create(crosspostChannel);
 				} else {
 					ServerDataHandler.unwriteChannel(guild.getId(), foundChannel);
-					this.sendMessage(channel, MessageUtil.createSuccessfulMessage("This channel now no longer crossposts to " + crosspostChannel.getAsMention()));
+					context.getFeedbackHandler().sendSuccess("This channel now no longer crossposts to " + crosspostChannel.getAsMention());
 				}
 			} else {
 				if (crosspostChannelId == channel.getIdLong()) {
-					this.sendMessage(channel, MessageUtil.createErrorMessage("Cannot make channel crosspost to itself"));
-					return;
+					throw CANNOT_CROSSPOST_TO_SELF_EXCEPTION.create();
 				}
 					
 				ServerData data = ServerDataHandler.getServerData(guild.getId());
 				if (data != null) {
 					for (ChannelData channelData : data.channelData) {
 						if (channelData.channelId == crosspostChannelId) {
-							this.sendMessage(channel, MessageUtil.createErrorMessage("Channel you want to crosspost to already crossposts to another channel"));
-							return;
+							throw ALREADY_CROSSPOSTS_TO_EXCEPTION.create(guild.getTextChannelById(channelData.channelId));
 						}
 					}
 				}
-					
-				this.sendMessage(channel, MessageUtil.createSuccessfulMessage("This channel now crossposts images to " + crosspostChannel.getAsMention()));
+
+				context.getFeedbackHandler().sendSuccess("This channel now crossposts images to " + crosspostChannel.getAsMention());
 				ServerDataHandler.writeChannel(guild.getId(), new ChannelData(channel.getIdLong(), crosspostChannelId), channel);
 			}
 		}
--- src/main/java/crossposter/commands/RequireAttachmentCommand.java
+++ src/main/java/crossposter/commands/RequireAttachmentCommand.java
@@ -3,22 +3,23 @@ package crossposter.commands;
 import crossposter.ServerDataHandler;
 import crossposter.ServerDataHandler.ChannelData;
 import crossposter.ServerDataHandler.ServerData;
-import disparser.Command;
-import disparser.CommandContext;
-import disparser.MessageUtil;
-import disparser.arguments.primitive.BooleanArgument;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.smelly.disparser.Command;
+import net.smelly.disparser.CommandContext;
+import net.smelly.disparser.arguments.java.BooleanArgument;
+import net.smelly.disparser.feedback.exceptions.SimpleCommandExceptionCreator;
 
 public class RequireAttachmentCommand extends Command {
+	private static final SimpleCommandExceptionCreator CHANNEL_CROSSPOST_EXCEPTION = new SimpleCommandExceptionCreator("This channel doesn't crosspost");
 
 	public RequireAttachmentCommand() {
 		super("require_attachment", BooleanArgument.get());
 	}
 
 	@Override
-	public void processCommand(CommandContext context) {
+	public void processCommand(CommandContext context) throws Exception {
 		GuildMessageReceivedEvent event = context.getEvent();
 		TextChannel channel = event.getChannel();
 		Guild guild = event.getGuild();
@@ -27,12 +28,11 @@ public class RequireAttachmentCommand extends Command {
 		if (serverData != null && !serverData.channelData.isEmpty()) {
 			ChannelData channelData = ChannelData.getCrosspostChannel(serverData.channelData, channel.getIdLong());
 			if (channelData != null) {
-				this.sendMessage(channel, MessageUtil.createSuccessfulMessage("This channel's messages " + (requireAttachment ? "now require an attachment to crosspost" : "no longer require an attachment to crosspost")));
+				context.getFeedbackHandler().sendSuccess("This channel's messages " + (requireAttachment ? "now require an attachment to crosspost" : "no longer require an attachment to crosspost"));
 				ServerDataHandler.writeChannel(guild.getId(), new ChannelData(channelData.channelId, channelData.crosspostChannelId, requireAttachment));
 				return;
 			}
 		}
-		this.sendMessage(channel, MessageUtil.createErrorMessage("This channel doesn't crosspost"));
+		throw CHANNEL_CROSSPOST_EXCEPTION.create();
 	}
-
 }
